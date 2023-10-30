92802c29918e164f6269215935e43706002b1c81
holfizz <gorlach7v@gmial.com>
2023-10-30 20:09:44 +0300

    init decomposition of webpack config

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,32 +1,18 @@
-import path from "path";
-import HTMLWebpackPlugin from "html-webpack-plugin";
 import webpack from "webpack";
+import path from "path";
+import { BuildPaths } from "./config/build/types/config";
+import { buildWebpackConfig } from "./config/build/buildWebpackConfig";
 
-const config: webpack.Configuration = {
-  mode: "development",
+const paths: BuildPaths = {
   entry: path.resolve(__dirname, "src", "index.ts"),
-  output: {
-    filename: "[name].[contenthash].js",
-    path: path.resolve(__dirname, "build"),
-    clean: true,
-  },
-  plugins: [
-    new HTMLWebpackPlugin({
-      template: path.resolve(__dirname, "public", "index.html"),
-    }),
-    new webpack.ProgressPlugin(),
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: "ts-loader",
-        exclude: /node_modules/,
-      },
-    ],
-  },
-  resolve: {
-    extensions: [".tsx", ".ts", ".js"],
-  },
+  build: path.resolve(__dirname, "build"),
+  html: path.resolve(__dirname, "public", "index.html"),
 };
+const mode = "development";
+const isDev = mode === "development";
+const config: webpack.Configuration = buildWebpackConfig({
+  isDev: isDev,
+  mode: "development",
+  paths,
+});
 export default config;
