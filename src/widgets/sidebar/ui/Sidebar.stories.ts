02b4b9ecbe667e2bef4c5fd1334ad97b81ec1543
holfizz <gorlach7v@gmial.com>
2023-11-12 11:41:37 +0300

    add loki reg-cli fix storybook

--- src/widgets/sidebar/ui/Sidebar.stories.ts
+++ src/widgets/sidebar/ui/Sidebar.stories.ts
@@ -1,7 +1,7 @@
 import type {Meta, StoryObj} from "@storybook/react"
 import {ThemeDecorator} from "shared/config/storybook/ThemeDecorator/ThemeDecorator"
 import {Theme} from "app/providers/ThemeProvider"
-import {Sidebar} from "widgets/sidebar"
+import Sidebar from "./Sidebar"
 
 const meta = {
     title: "widgets/sidebar",
@@ -14,10 +14,10 @@ export default meta
 type Story = StoryObj<typeof meta>;
 
 
-export const Light: Story = {
+export const LightSidebar: Story = {
 }
 
 
-export const Dark: Story = {
+export const DarkSidebar: Story = {
     decorators: [ThemeDecorator(Theme.DARK)],
 }
--- src/widgets/themeSwitcher/ui/ThemeSwitcher.tsx
+++ src/widgets/themeSwitcher/ui/ThemeSwitcher.tsx
@@ -1,10 +1,10 @@
-import type { FC } from "react"
-import { classNames } from "shared/lib/classNames/classNames"
+import type {FC} from "react"
+import {classNames} from "shared/lib/classNames/classNames"
 import cls from "./ThemeSwitcher.module.scss"
-import { Theme, useTheme } from "app/providers/ThemeProvider"
-import { CSSTransition } from "react-transition-group"
+import {Theme, useTheme} from "app/providers/ThemeProvider"
+import {CSSTransition} from "react-transition-group"
 import Button from "shared/ui/Button/Button"
-import { Moon, Sun } from "lucide-react"
+import {Moon, Sun} from "lucide-react"
 
 interface ThemeSwitcherProps {
   className?: string;
@@ -19,21 +19,9 @@ const ThemeSwitcher: FC<ThemeSwitcherProps> = ({ className }) => {
             className={classNames(cls.ThemeSwitcher, {}, [className])}
         >
             <CSSTransition
-                in={theme === Theme.DARK}
-                timeout={300}
-                unmountOnExit
-                classNames={{
-                    enter: cls.sunMoonEnter,
-                    enterActive: cls.sunMoonEnterActive,
-                    exit: cls.sunMoonExit,
-                    exitActive: cls.sunMoonExitActive,
-                }}
-            >
-                <Moon />
-            </CSSTransition>
-            <CSSTransition
-                in={theme === Theme.LIGHT}
+                in={!!theme}
                 timeout={400}
+                mountOnEnter
                 unmountOnExit
                 classNames={{
                     enter: cls.sunMoonEnter,
@@ -42,7 +30,7 @@ const ThemeSwitcher: FC<ThemeSwitcherProps> = ({ className }) => {
                     exitActive: cls.sunMoonExitActive,
                 }}
             >
-                <Sun />
+                {theme === Theme.DARK ? <Sun /> : <Moon />}
             </CSSTransition>
         </Button>
     )
