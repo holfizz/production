8b8578da7b15e46a0e2ece7822a38df907847157
holfizz <gorlach7v@gmial.com>
2023-12-11 19:27:09 +0300

    add jest alias, add theme addon for storybook

--- config/storybook/preview.ts
+++ config/storybook/preview.ts
@@ -4,7 +4,7 @@ import { StyleDecorator } from "../../src/shared/config/storybook/StyleDecorator
 import { ThemeDecorator } from "../../src/shared/config/storybook/ThemeDecorator/ThemeDecorator"
 import { TranslationDecorator } from "../../src/shared/config/storybook/TranslationDecorator/TranslationDecorator"
 import { SuspenseDecorator } from "../../src/shared/config/storybook/SuspenseDecorator/SuspenseDecorator"
-import { Theme } from "../../src"
+import { Theme } from "../../src/shared/const/theme"
 
 const preview: Preview = {
     parameters: {
@@ -15,6 +15,13 @@ const preview: Preview = {
                 date: /Date$/i,
                 layout: "centered",
             },
+        },
+        themes: {
+            default: "twitter",
+            list: [
+                { name: "light", class: Theme.LIGHT, color: "#e5e5f8" },
+                { name: "dark", class: Theme.DARK, color: "#171515" },
+            ],
         },
     },
     decorators: [
