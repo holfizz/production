96dd86caef9a2781a2e3c92fff00d464b6217acb
holfizz <gorlach7v@gmial.com>
2023-12-17 11:34:33 +0300

    get api url function

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,32 +1,41 @@
 import webpack from "webpack"
 import * as path from "path"
 import {buildWebpackConfig} from "./config/build/buildWebpackConfig"
-import {BuildEnv, BuildPaths} from "./config/build/types/config"
+import {BuildEnv, BuildMode, BuildPaths} from "./config/build/types/config"
 
-export default (env: BuildEnv) => {
-    const paths: BuildPaths = {
-        entry: path.resolve(__dirname, "src", "index.tsx"),
-        build: path.resolve(__dirname, "build"),
-        html: path.resolve(__dirname, "public", "index.html"),
-        src: path.resolve(__dirname, "src"),
-        locales: path.resolve(__dirname, "public", "locales"),
-        buildLocales: path.resolve(__dirname, "build", "locales"),
-    }
-
-    const mode = env?.mode || "development"
-    const PORT = env?.port || 3000
-    const apiUrl = env?.apiUrl ||"https://89.248.204.68:8443"
-
-    const isDev = mode === "development"
-
-    const config: webpack.Configuration = buildWebpackConfig({
-        mode,
-        paths,
-        isDev,
-        apiUrl: apiUrl,
-        port: PORT,
-        project: "frontend",
-    })
-
-    return config
+function getApiUrl(mode: BuildMode, apiUrl?: string) {
+  if (apiUrl) {
+    return apiUrl;
+  }
+  if (mode === "production") {
+    return "/api";
+  }
+  return "http://localhost:8000";
 }
+export default (env: BuildEnv) => {
+  const paths: BuildPaths = {
+    entry: path.resolve(__dirname, "src", "index.tsx"),
+    build: path.resolve(__dirname, "build"),
+    html: path.resolve(__dirname, "public", "index.html"),
+    src: path.resolve(__dirname, "src"),
+    locales: path.resolve(__dirname, "public", "locales"),
+    buildLocales: path.resolve(__dirname, "build", "locales"),
+  };
+
+  const mode = env?.mode || "development";
+  const PORT = env?.port || 3000;
+  const apiUrl = getApiUrl(mode, env?.apiUrl);
+
+  const isDev = mode === "development";
+
+  const config: webpack.Configuration = buildWebpackConfig({
+    mode,
+    paths,
+    isDev,
+    apiUrl: apiUrl,
+    port: PORT,
+    project: "frontend",
+  });
+
+  return config;
+};
