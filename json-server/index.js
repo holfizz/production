3f433c3a403af1d3313d1d44f35ed209f038c829
holfizz <gorlach7v@gmial.com>
2023-12-17 09:43:44 +0300

    change port

--- json-server/index.js
+++ json-server/index.js
@@ -1,60 +1,62 @@
-const fs = require('fs')
-const jsonServer = require('json-server')
-const https = require('node:https');
+const fs = require("fs");
+const jsonServer = require("json-server");
+const https = require("node:https");
+const path = require("path");
 
 const options = {
-    key: fs.readFileSync(path.resolve(__dirname, "key.pem")),
-    cert: fs.readFileSync(path.resolve(__dirname, "cert.pem")),
+  key: fs.readFileSync(path.resolve(__dirname, "key.pem")),
+  cert: fs.readFileSync(path.resolve(__dirname, "cert.pem")),
 };
-const path = require('path')
 
-const server = jsonServer.create()
+const server = jsonServer.create();
 
-const router = jsonServer.router(path.resolve(__dirname, 'db.json'))
+const router = jsonServer.router(path.resolve(__dirname, "db.json"));
 
-server.use(jsonServer.defaults({}))
-server.use(jsonServer.bodyParser)
+server.use(jsonServer.defaults({}));
+server.use(jsonServer.bodyParser);
 
 server.use(async (req, res, next) => {
-    await new Promise((res) => {
-        setTimeout(res, 800)
-    })
-    next()
-})
+  await new Promise((res) => {
+    setTimeout(res, 800);
+  });
+  next();
+});
 
-server.post('/login', (req, res) => {
-    try {
-        const { username, password } = req.body
-        const db = JSON.parse(fs.readFileSync(path.resolve(__dirname, 'db.json'), 'UTF-8'))
-        const { users = [] } = db
+server.post("/login", (req, res) => {
+  try {
+    const { username, password } = req.body;
+    const db = JSON.parse(
+      fs.readFileSync(path.resolve(__dirname, "db.json"), "UTF-8")
+    );
+    const { users = [] } = db;
 
-        const userFromBd = users.find(
-            (user) => user.username === username && user.password === password,
-        )
+    const userFromBd = users.find(
+      (user) => user.username === username && user.password === password
+    );
 
-        if (userFromBd) {
-            return res.json(userFromBd)
-        }
-
-        return res.status(403).json({ message: 'User not found' })
-    } catch (e) {
-        console.log(e)
-        return res.status(500).json({ message: e.message })
+    if (userFromBd) {
+      return res.json(userFromBd);
     }
-})
+
+    return res.status(403).json({ message: "User not found" });
+  } catch (e) {
+    console.log(e);
+    return res.status(500).json({ message: e.message });
+  }
+});
 
 server.use((req, res, next) => {
-    if (!req.headers.authorization) {
-        return res.status(403).json({ message: 'AUTH ERROR' })
-    }
+  if (!req.headers.authorization) {
+    return res.status(403).json({ message: "AUTH ERROR" });
+  }
 
-    next()
-})
+  next();
+});
 
-server.use(router)
+server.use(router);
 
-const PORT = 8443
-const httpsServer = https.createServer(options, server)
+const PORT = 8443;
+const httpsServer = https.createServer(options, server);
 httpsServer.listen(PORT, () => {
-    console.log(`server is running on ${PORT} port`)
-})
+  console.log(`server is running on ${PORT} port`);
+});
