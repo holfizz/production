16915ff5d823f43b9eb12d581ff9a4fb93146de0
holfizz <gorlach7v@gmial.com>
2023-12-17 10:38:21 +0300

    change port

--- webpack.config.ts
+++ webpack.config.ts
@@ -15,7 +15,7 @@ export default (env: BuildEnv) => {
 
     const mode = env?.mode || "development"
     const PORT = env?.port || 3000
-    const apiUrl = "https://89.248.204.68:8443"
+    const apiUrl = env?.apiUrl ||"https://89.248.204.68:8443"
 
     const isDev = mode === "development"
 
